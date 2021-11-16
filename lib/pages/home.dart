80cbbd7e80d519cd0460244e88764259831b29ac
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-11-17 00:40:16 +0800

    [UPDATE] fixed some lint warning

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,10 +5,8 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Rainforest'),
-      ),
+    return const Center(
+      child: Text('Rainforest'),
     );
   }
 }
